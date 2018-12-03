e05df0a2ff57f8b60a4f2b5701ba65185670193e
Bizley <pawel@positive.codes>
2018-12-03 15:20:05 +0100

    admin footer

--- src/views/layouts/footer.php
+++ src/views/layouts/footer.php
@@ -1,4 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/* @var $this \yii\web\View */
+
+$currentYear = (int) date('Y');
+$startYear = 2018;
+
+$period = $startYear;
+if ($currentYear > $startYear) {
+    $period .= '-' . $currentYear;
+}
+
+?>
 <hr>
 <footer>
-    <p>&copy; 2016 Company, Inc.</p>
+    <p>&copy; <?= $period ?> Podium v<?= $this->context->module->getVersion() ?></p>
 </footer>
