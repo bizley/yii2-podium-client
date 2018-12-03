227e56016eb00fdf71d274adddd8ec80f8efb9c2
Bizley <pawel@positive.codes>
2018-12-03 16:30:54 +0100

    admin panels

--- src/views/admin/index.php
+++ src/views/admin/index.php
@@ -4,9 +4,11 @@
     </p>
     <div class="row">
         <div class="col-xs-6 col-lg-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h2>Users</h2>
+                </div>
+            </div>
         </div><!--/.col-xs-6.col-lg-4-->
         <div class="col-xs-6 col-lg-4">
             <h2>Heading</h2>
