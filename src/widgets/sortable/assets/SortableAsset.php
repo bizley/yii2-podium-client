df414ceb7270776b3fae2f47ca0a70dfc4d54e78
Paweł Brzozowski <pawel@positive.codes>
2019-03-09 11:51:26 +0100

    Sortable asset

--- src/widgets/sortable/assets/SortableAsset.php
+++ src/widgets/sortable/assets/SortableAsset.php
@@ -2,11 +2,41 @@
 
 declare(strict_types=1);
 
-
 namespace bizley\podium\client\widgets\sortable\assets;
 
+use yii\web\AssetBundle;
+use yii\web\JqueryAsset;
 
-class SortableAsset
+/**
+ * Class SortableAsset
+ * @package bizley\podium\client\widgets\sortable\assets
+ *
+ * https://sortablejs.github.io/Sortable/
+ * https://github.com/SortableJS/jquery-sortablejs
+ */
+class SortableAsset extends AssetBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@npm/jquery-sortablejs/';
 
+    /**
+     * {@inheritdoc}
+     */
+    public $js = ['jquery-sortable.min.js'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $publishOptions = [
+        'only' => [
+            'jquery-sortable.min.js'
+        ],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $depends = [JqueryAsset::class];
 }
